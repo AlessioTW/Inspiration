a1a03d66920c73bc3fbb329907d7357b5f4501d9
matteo-tita <matteo.tita@gmail.com>
2025-06-25 14:47:03 +0200

    Struttura progetto sistemata

--- app/src/main/java/com/example/inspiration/InspirationScreen.kt
+++ app/src/main/java/com/example/inspiration/InspirationScreen.kt
@@ -1,2 +1,9 @@
 package com.example.inspiration
 
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavHostController
+
+@Composable
+fun InspirationApp (
+    viewModel: InspirationViewModel = viewModel(), navController: NavHostController = rememberNavController())
